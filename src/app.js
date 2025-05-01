aa4e0a542faaca5fd92eb2b14576fcfac3f95592
Elflight <Elflight@yandex.ru>
2025-05-01 18:48:05 +0300

    step 4

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ import onChange from 'on-change';
 import * as i18next from 'i18next';
 import ruTranslation from './locales/ru.json';
 import * as yup from 'yup';
-import {renderForm} from './view.js';
+import axios from 'axios';
+import {renderForm, renderContent} from './view.js';
+import parseRss from './parser.js';
 
 export default () => {
     i18next.init({
@@ -15,12 +17,12 @@ export default () => {
 
     yup.setLocale({
         mixed: {
-          default: () => i18next.t('errors.default'),
-          required: () => i18next.t('errors.required'),
-          notOneOf: () => i18next.t('errors.duplicate'),
+          default: () => i18next.t('formErrors.default'),
+          required: () => i18next.t('formErrors.required'),
+          notOneOf: () => i18next.t('formErrors.duplicate'),
         },
         string: {
-          url: () => i18next.t('errors.invalidUrl'),
+          url: () => i18next.t('formErrors.invalidUrl'),
         },
     });
 
@@ -29,12 +31,20 @@ export default () => {
         formInput: document.querySelector('#url-input'),
         formBtn: document.querySelector('.rss-form button[type="submit"]'),
         errField: document.querySelector('.feedback'),
+        main: document.querySelector('main'),
     };
 
     const formStates = {
         BASE: "base",
         LOADING: "loading"
     };
+
+    const loadingStates = {
+        BASE: "base",
+        LOADING: "loading"
+    };
+
+    let feedID = 0;
 
     const state = {
         form: {
@@ -46,13 +56,15 @@ export default () => {
             status: "",
             error: "",
         },
-        feeds: [
-        ]
-    }; 
+        feeds: [],
+        posts: {}
+    };
 
     const watchedState = onChange(state, function (path, value, previousValue, applyData) {
         if(path.startsWith("form")) {
             renderForm(watchedState.form, pageElements, formStates);
+        } else if(path === 'feeds') {
+            renderContent(watchedState.feeds, watchedState.posts, i18next, pageElements);
         }
     });
 
@@ -60,26 +72,79 @@ export default () => {
         event.preventDefault();
         watchedState.form.state = formStates.LOADING;
 
-        const val = pageElements.formInput.value;
+        const url = pageElements.formInput.value.trim();
+
+        const test = watchedState.feeds.map((feed) => feed.url);
+        console.log(watchedState.feeds);
+        console.log('test', test);
+        console.log('url', url);
         
-        const rssFieldSchema = yup.string().trim().required().url().notOneOf(watchedState.feeds.map((feed) => feed.url));
-        rssFieldSchema.validate(val)
-        .then(() => {
+        const rssFieldSchema = yup.string()
+        .required()
+        .url()
+        .notOneOf(watchedState.feeds.map((feed) => feed.url));
+
+        rssFieldSchema.validate(url) //валидируем адрес
+        .then(() => { //загружаем данные
             watchedState.form = { ...watchedState.form, isValid: true, error: ""};
-            getFeedData(val)
-            .then(() => {
-                console.log(watchedState);
-                watchedState.form.state = formStates.BASE;
+            return getFeedData(url);
+        })
+        .then((feedData) => { //проверяем корректность
+            return getRss(feedData);
+        })
+        .then((rawRss) => { //парсим
+            return parseRss(rawRss) //пришлось обернуть в отдельный блок, чтобы перехватить ошибку из парсера и обработать её через i18n
+            .then((parsedRss) => {
+                return parsedRss;
+            }).catch((err) => {
+                err.message = i18next.t(err.message);
+                throw err;
             });
         })
+        .then((parsedRss) => { //записываем данные в state
+            watchedState.form.state = formStates.BASE;
+            watchedState.loadingProcess.status = loadingStates.BASE;
+
+            const currentFeedID = ++feedID;
+            watchedState.posts['feed'+currentFeedID] = parsedRss.posts;
+            watchedState.feeds.push({...parsedRss.feed, id: currentFeedID, url});
+            // console.log(watchedState);
+        })
         .catch((err) => {
-            Object.assign(watchedState.form, {})
             watchedState.form = { ...watchedState.form, isValid: false, error: err.message, state: formStates.BASE};
-        });
+        })
+        // .finally(() => {
+        //     watchedState.form.state = formStates.BASE;
+        //     watchedState.loadingProcess.status = loadingStates.BASE;
+        // });
     });
 
-    const getFeedData = (val, ms=5000) => new Promise(resolve => {
-        watchedState.feeds.push({name: "test", url: val});
-        setTimeout(resolve, ms);
-    });
+    //получаем проксированный адрес и делаем запрос
+    const getFeedData = (url) => {
+        watchedState.loadingProcess.status = loadingStates.LOADING;
+
+        const proxedUrl =`https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}`;
+
+        //загружаем содержимое
+        return axios.get(proxedUrl)
+            .then((response) => {
+                return response; // возвращаем данные, чтобы использовать их потом
+            })
+            .catch((error) => {
+                watchedState.loadingProcess.error = error.message;
+                throw error; // пробрасываем ошибку выше
+            });
+    };
+
+    //проверяем, что получен именно RSS и возвращаем именно тело
+    const getRss = (feedData) => {
+        return new Promise((resolve, reject) => {
+          const contentType = feedData.data.status.content_type;
+          if (contentType === "application/rss+xml; charset=utf-8") {
+            resolve(feedData.data.contents); // всё ок — передаём дальше
+          } else {
+            reject(new Error(i18next.t('loader.invalidRss')));
+          }
+        });
+    };
 }
--- src/view.js
+++ src/view.js
@@ -18,6 +18,51 @@ const renderForm = (formstate, pageElements, formStates) => {
     }
 
     pageElements.errField.textContent = formstate.error ? formstate.error : "";
+    pageElements.errField.classList.remove('text-success');
+    pageElements.errField.classList.add('text-danger');
 };
 
-export {renderForm};
+const renderContent = (feeds, posts, i18next, pageElements) => {
+    const contentWrapper = document.querySelector('#contentWrapper');
+    if(contentWrapper) {
+        contentWrapper.remove();
+    }
+
+    let html = `<section class="container-fluid container-xxl p-5" id="contentWrapper">
+        <div class="row">
+            <div class="col-md-10 col-lg-8 order-1 mx-auto posts">
+                <div class="card border-0">
+                    <div class="card-body"><h2 class="card-title h4">${i18next.t("posts.header")}</h2></div>
+                    <ul class="list-group border-0 rounded-0">`;
+   
+    Object.entries(posts).forEach(([feedKey, feedPosts]) => {
+        feedPosts.forEach((feedPost, index) => {
+            html += `<li class="list-group-item d-flex justify-content-between align-items-start border-0 border-end-0">`;
+            html += `<a href="${feedPost.link}" class="fw-bold" data-id="${feedKey}" target="_blank" rel="noopener noreferrer">${feedPost.title}</a>`;
+            html += `<button type="button" class="btn btn-outline-primary btn-sm" data-id="${feedKey}" data-bs-toggle="modal" data-bs-target="#modal">${i18next.t("posts.button")}</button>`;
+            html += `</li>`;
+        });
+    });
+    html += `</ul></div></div>`;
+
+    html += `<div class="col-md-10 col-lg-4 mx-auto order-0 order-lg-1 feeds">
+                <div class="card border-0">
+                    <div class="card-body"><h2 class="card-title h4">${i18next.t("feeds.header")}</h2></div>
+                    <ul class="list-group border-0 rounded-0">`;
+    feeds.forEach((objFeed) => {
+        html += `<li class="list-group-item border-0 border-end-0">
+                    <h3 class="h6 m-0">${objFeed.title}</h3>
+                    <p class="m-0 small text-black-50">${objFeed.description}</p>
+                </li>`;
+    });
+    html += `</ul></div></div>`;
+    html += `</div></section>`;
+
+    pageElements.main.insertAdjacentHTML('beforeend', html);
+
+    pageElements.errField.textContent = i18next.t("loader.rssSuccess");
+    pageElements.errField.classList.remove('text-danger');
+    pageElements.errField.classList.add('text-success');
+}
+
+export {renderForm, renderContent};
